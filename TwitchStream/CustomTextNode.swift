a13fba7059453abfb1278dcb8cd4eedbd39fb809
Taufik Obet <mailtaufik@gmail.com>
2017-11-24 18:35:18 +0700

    Create new CustomTextNode

---
+++ TwitchStream/CustomTextNode.swift
@@ -0,0 +1,34 @@
+//
+//  CustomTextNode.swift
+//  TwitchStream
+//
+//  Created by Taufik Obet on 24/11/17.
+//  Copyright © 2017 spacetimecorp. All rights reserved.
+//
+
+import Foundation
+import AsyncDisplayKit
+
+class CustomTextNode: ASControlNode {
+    
+    let liveLabelNode = ASTextNode()
+    let backgroundNode = ASDisplayNode()
+
+    override init() {
+        super.init()
+
+        backgroundNode.backgroundColor = UIColor.red
+        addSubnode(backgroundNode)
+
+        addSubnode(liveLabelNode)
+        liveLabelNode.attributedText = NSAttributedString(string: "LIVE", attributes: [NSAttributedStringKey.foregroundColor:UIColor.white, NSAttributedStringKey.font: UIFont.systemFont(ofSize: 16)])
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        let liveLabelInsets = ASInsetLayoutSpec(insets: UIEdgeInsetsMake(0, 4, 2, 4), child: liveLabelNode)
+        let backgroundSpec = ASBackgroundLayoutSpec(child: liveLabelInsets, background: backgroundNode)
+        let liveLabelStack = ASStackLayoutSpec(direction: .horizontal, spacing: 0, justifyContent: .end, alignItems: .start, children: [backgroundSpec])
+        let liveLabelStackInset = ASInsetLayoutSpec(insets: UIEdgeInsetsMake(8, 8, 8, 8), child: liveLabelStack)
+        return liveLabelStackInset
+    }
+}
